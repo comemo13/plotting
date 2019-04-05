89ef8b943732a4e158a6bc36f7de78eaf45b0356
comemo13 <colleen.mossor@portlandoregon.gov>
2019-04-05 11:31:05 -0700

    continued with dplyr examples

--- code/intro-dplyr.R
+++ code/intro-dplyr.R
@@ -2,7 +2,7 @@
 library(dplyr)
 
 # load gapminder dataset
-gapminder <-  read.csv("gapminder_data.csv",
+gapminder <-  read.csv("data/gapminder_data.csv",
                        stringsAsFactors = F)
 
 #gapminder$continent <- as.factor(gapminder$continent)
@@ -56,3 +56,72 @@ europe <- gapminder %>%
 # assign table as object and view to look better
 europe_table <- table(europe$country)
 View(europe_table) #capital V
+
+#working through the dplyr cheat sheet
+tbl_df(europe)
+glimpse(europe)
+View(europe)
+
+# working with group_by() and summarize ()
+str(gapminder %>%  group_by(continent))
+# group_by alters the dataframe to treat it as a list or as
+# separate mini data frames within your data frame
+# it subsets it without making different objects
+
+# summarize mean gdp per continent
+gdp_continent <- gapminder %>% 
+  group_by(continent) %>% 
+  summarize(mean_gdp = mean(gdpPercap),
+  mean_lifeExp = mean(lifeExp))
+  # summarize creates a new object and for each 
+  #continent have a column with the mean gdp per cap
+View(gdp_continent)
+
+# plot it
+library(ggplot2)
+summary_plot <- gdp_continent %>%
+  ggplot(aes(x = mean_gdp, y=mean_lifeExp))+
+  geom_point(stat = "identity") +
+  theme_bw()
+summary_plot  
+
+# exercise - calc mean pop for all continents
+pop_continent <- gapminder %>% 
+  group_by(continent) %>% 
+  summarize(mean_pop = mean(pop))
+View(pop_continent)
+
+# exercise - calc mean pop for all continents for most recent year
+pop_cont_curr <- gapminder %>% 
+  group_by(continent) %>% 
+  filter(year == max(year)) %>% 
+  summarize(mean_pop = mean(pop))
+View(pop_cont_curr)
+
+# count() and n()
+# count is like frequency
+gapminder %>% 
+  filter(year == 2002) %>% 
+  count(continent, sort = T) #give number of countries in each continent for that year
+
+gapminder %>% 
+  group_by(continent) %>% 
+  summarize(se = sd(lifeExp)/sqrt(n()))
+  # useful for when you dont know what the n value is, it looks
+  # it up for you
+
+# mutate() is my friend
+# when you're going to create a brand new column to add to the dataframe
+xy <- data.frame(x= rnorm(100), 
+                 y= rnorm(100))
+head(xy)
+xyz <-  xy %>% 
+  mutate(z=x*y)
+head(xyz)
+
+# add col that gives full gdp for the country and summarize by continent
+gdp_per_cont <- gapminder %>% 
+  mutate(country_gdp=gdpPercap*pop) %>% 
+  group_by(continent) %>% 
+  summarize(total_gdp = sum(country_gdp))
+View(gdp_per_cont)

127c41962f98c17ff89db9e5d324a38b981da289
comemo13 <colleen.mossor@portlandoregon.gov>
2019-04-08 10:37:34 -0700

    altered the original far_to_kel function to add defensive prog. Then also added new far_to_cel3.1 and far_to_cel3.2 to practice more

--- code/functions.r
+++ code/functions.r
@@ -6,12 +6,18 @@
 # argument is the required input, defined here as "temp"
 # the "return" is optional in R, it will auto return the last object, but it's a good habit
 # you can pipe within a function
+
+# came back later to add defensive
 far_to_kel <- function(temp){
+  if(!is.numeric(temp)){
+    stop("value must be numeric")
+  }
+  
   kelvin <- ((temp-32)*(5/9))+273.15
   return(kelvin)
 }
 
-far_to_kel(0)
+far_to_kel(NA)
 far_to_kel(32)
 
 # my practice - Kelvin to Celcius
@@ -50,3 +56,29 @@ far_to_cel3(32)
 # Defensive programming is set to allow/not allow things, ie a function that needs an 
 # integer, prohibiting string
 # commit before changing the function
+# added defensive programming to the original far_To_kel
+far_to_kel2 <- function(temp){
+  stopifnot(is.numeric(temp))
+  kelvin <- ((temp-32)*(5/9))+273.15
+  return(kelvin)
+}
+far_to_kel2(NA)
+
+# Create defensive stop for far_to_cel
+far_to_cel3.1 <-  function(temp){
+  stopifnot(is.numeric(temp))
+  kel <- far_to_kel(temp)
+  cel <- kel_to_cel(kel)
+  return(cel)
+}
+far_to_cel3.1(NA)
+far_to_cel3.2 <-  function(temp){
+  if (!is.numeric(temp)) {
+    stop("not numeric")
+  }
+  kel <- far_to_kel(temp)
+  cel <- kel_to_cel(kel)
+  return(cel)
+}
+far_to_cel3.2(NA)
+# commit after changing the functions. 

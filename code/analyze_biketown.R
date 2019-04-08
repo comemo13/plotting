13bc3ada3336a5c8c534f8509e6b9d5a2b816d25
comemo13 <colleen.mossor@portlandoregon.gov>
2019-04-08 14:57:34 -0700

    left notes about sourcing in analyze_biketown.R. used piping in fetch_biketown.R to show to put pipe named functions without actually having to name them.

--- code/analyze_biketown.R
+++ code/analyze_biketown.R
@@ -3,3 +3,7 @@
 
 source("code/fetch_biketown.R")
 get_data(start = "06/2018", end = "08/2018")
+
+# using the source option, as opposed to copy annd paste, is good for version control
+# now I can debug in the single origin of the R script, and by sourcing it in I only have 
+# the one, accurate script. 
--- code/fetch_biketown.R
+++ code/fetch_biketown.R
@@ -53,4 +53,64 @@ get_data <- function(start="7/2016", end=NULL, #adding defaults
 # get_data(start)
 # ^this works so I will commit
 
+# You can write a function inline. In this example we get rid of the FOR LOOP
+# because they aren't very effective, we replace it with another APPLY statement
+get_data2 <- function(start="7/2016", end=NULL, 
+                     base_url="https://s3.amazonaws.com/biketown-tripdata-public/",
+                     outdir="data/biketown/"){
 
+  end <- ifelse(is.null(end),format(now(),"%m/%Y"),end)
+  
+  make_url <- function(date, base_url){
+    url <- paste0(base_url, format(date,"%Y_%m"), ".csv") 
+    return(url)
+  }
+  
+  start_date <- lubridate::myd(start, truncated = 2)
+
+  end_date  <- myd(end, truncated = 2)
+  date_range <- seq(start_date, end_date, by="months")
+  
+  urls <- lapply(date_range, make_url, base_url=base_url)
+  
+  # for (u in urls) {
+  #   download.file(u, destfile = paste0(outdir, 
+  #                                      str_sub(u,-11)))
+  # }
+
+  # instead of assigning the function as a named function, we can make it inline
+    result <- lapply(urls, function(u) {
+    download.file(u, destfile = paste0(outdir, str_sub(u,-11)))
+  })
+}
+
+start = "11/2018"
+
+get_data2(start)
+
+# Next part is to try to get rid of the intermediate steps into one singluar piped version
+get_data3  <- function(start="7/2016", end=NULL, 
+                       base_url="https://s3.amazonaws.com/biketown-tripdata-public/",
+                       outdir="data/biketown/"){
+  
+  end <- ifelse(is.null(end),format(now(),"%m/%Y"),end)
+  
+  make_url <- function(date, base_url){
+    url <- paste0(base_url, format(date,"%Y_%m"), ".csv") 
+    return(url)
+  }
+  
+  start_date <- lubridate::myd(start, truncated = 2)
+  end_date  <- myd(end, truncated = 2)
+  date_range <- seq(start_date, end_date, by="months")
+  
+  lapply(date_range, make_url, base_url=base_url) %>% 
+    lapply(function(u) {
+    download.file(u, destfile = paste0(outdir, str_sub(u,-11)))
+  })
+}
+# pipe can only replace the first argument in a function. In the previous one, the 2nd lapply
+# referenced "urls" as the first arguement, now we are replacing that with the 1st lapply
+start = "11/2018"
+
+get_data3(start)
